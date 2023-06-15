3ce8f2d7cf7bc084e2c34083058fb54eadd0550a
Felix Lai <felix.lai@hotmail.com>
2023-06-15 10:00:27 -0400

    useonclickoutside

--- src/components/layout/project-layout/Header.tsx
+++ src/components/layout/project-layout/Header.tsx
@@ -1,3 +1,27 @@
+import React, { Fragment, useEffect, useRef, useState } from "react";
+
+export function useOnClickOutside<T extends HTMLDivElement>(
+  ref: React.RefObject<T>,
+  handler: (e: any) => void
+) {
+  useEffect(() => {
+    const listener = (event: any) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+}
+
 export const Header: React.FC = () => {
   return <div></div>;
 };
