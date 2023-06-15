4d15243c0471030f8fc9f60a2af8d477a2ea64fb
Felix Lai <felix.lai@hotmail.com>
2023-06-15 15:49:52 -0400

    updated colours

--- tailwind-preset.js
+++ tailwind-preset.js
@@ -14,4 +14,27 @@ module.exports = {
   variants: {
     animation: ["motion-safe"],
   },
+  theme: {
+    extend: {
+      colors: {
+        current: "currentColor",
+        brand: {
+          evenLighter: "#fcf7f8",
+          gold: "#AA8548",
+          lighter: "#738290",
+          base: "#42484D",
+          dark: "#2C2E2F",
+          darkest: "#272727",
+        },
+        ui: {
+          error: red[700],
+        },
+      },
+    },
+    textShadow: {
+      sm: "0 1px 10px var(--tw-shadow-color)",
+      DEFAULT: "0 2px 10px var(--tw-shadow-color)",
+      lg: "0 8px 16px var(--tw-shadow-color)",
+    },
+  },
 };
