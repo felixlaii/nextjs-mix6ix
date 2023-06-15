fccaa1c570114d83abccbd2434dedd077c427bcb
Felix Lai <felix.lai@hotmail.com>
2023-06-15 10:47:41 -0400

    added props

---
+++ src/pages/services.tsx
@@ -0,0 +1,17 @@
+export interface ServiceImageProps {
+  img: string;
+  name: string;
+  service: string;
+  description: string;
+}
+
+export interface ServiceLinkProps {
+  service: string;
+  description: string;
+  href: string;
+  img: string;
+  id: string;
+}
+export interface ServiceLinkArr {
+  links: ServiceLinkProps[];
+}
