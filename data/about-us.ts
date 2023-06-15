0aaf05356d18eccac7f37b766eaad964d8f0567f
Felix Lai <felix.lai@hotmail.com>
2023-06-15 12:54:00 -0400

    Create about-us.ts

---
+++ data/about-us.ts
@@ -0,0 +1,10 @@
+export const MIX_ABOUT: {
+  title: string;
+  description: string;
+}[] = [
+  {
+    title: "About Mix6ix",
+    description:
+      "A team of passionate, energetic, and experienced hospitality professionals who specialize in creating the most memorable cocktail experience for our guests. Our vision is to offer a culture through craftsmanship, and a personalized and exclusive experience. From intimate gatherings to large celebration parties. We are a bartending company serving within the Greater Toronto Area. ",
+  },
+];
