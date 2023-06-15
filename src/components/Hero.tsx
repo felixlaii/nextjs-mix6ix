b00ed3a393bb877beaaec703165865518b80c4e4
Felix Lai <felix.lai@hotmail.com>
2023-06-15 10:43:37 -0400

    added hero image

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+const Hero: React.FC = () => {
+  return (
+    <div>
+      <img
+        src="/images/hero-image.jpeg"
+        className="w-full xl:h-[38rem] z-0 object-cover contrast-more"
+        alt="Hero Image"
+      />
+    </div>
+  );
+};
+
+export default Hero;
