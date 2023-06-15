b35084c17c008c86a10086c79058de8f92d2767e
Felix Lai <felix.lai@hotmail.com>
2023-06-15 10:14:18 -0400

    added logo

--- src/components/layout/ProjectLayout.tsx
+++ src/components/layout/ProjectLayout.tsx
@@ -1,11 +1,13 @@
 import { PropsWithChildren } from "react";
 import { Content } from "./project-layout/Content";
 import Footer from "./project-layout/Footer";
-import { Header } from "./project-layout/Header";
+import Header from "./project-layout/Header";
 import { Wrapper } from "./project-layout/Wrapper";
 import { useRouter } from "next/router";
 import { SERVICES_DROPDOWN } from "../../../data/services";
 import { NavigationLink } from "@/types/component-types";
+import logo from "../../../public/images/MIX6IX2022.png";
+import clsx from "clsx";
 
 /**
  * Responsive web UI layout for RheumInfo.
@@ -30,7 +32,18 @@ export const ProjectLayout: React.FC<PropsWithChildren> = (
 
   return (
     <Wrapper>
-      <Header />
+      <Header
+        logo={logo.src}
+        navigationLinks={navigationLinks}
+        currentActiveLocation={location.pathname}
+        textClassName="group text-md transition-all duration-300 ease-in-out text-black font-light mx-8 text-zinc-600"
+        linkClassName="flex bg-left-bottom lg:text-md"
+        logoClassName="sm:w-3/5 sm:ml-8 md:w-3/4 ml-2 mt-2 mr-1"
+        hoverClassName={clsx(
+          "flex bg-left-bottom hover:text-brand-lightest bg-gradient-to-r from-brand-lightest/40 to-brand-darkest bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+        )}
+        activeLinkClassName="text-brand-lightest font-normal lg:text-lg font-light"
+      />
       <Content>{children}</Content>
       <Footer />
     </Wrapper>
