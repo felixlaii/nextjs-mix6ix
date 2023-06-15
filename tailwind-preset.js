ea1e36265331b0400534446566ed8cc04985261a
Felix Lai <felix.lai@hotmail.com>
2023-06-15 15:21:30 -0400

    added variants animation

--- tailwind-preset.js
+++ tailwind-preset.js
@@ -9,3 +9,9 @@ const round = (num) =>
     .replace(/\.0$/, "");
 const rem = (px) => `${round(px / 16)}rem`;
 const em = (px, base) => `${round(px / base)}em`;
+
+module.exports = {
+  variants: {
+    animation: ["motion-safe"],
+  },
+};
