14eb332d4f9664789bbb3621931c50d0579ce555
Felix Lai <felix.lai@hotmail.com>
2023-06-15 16:11:46 -0400

    added zindex and plugins

--- tailwind-preset.js
+++ tailwind-preset.js
@@ -36,5 +36,25 @@ module.exports = {
       DEFAULT: "0 2px 10px var(--tw-shadow-color)",
       lg: "0 8px 16px var(--tw-shadow-color)",
     },
+    zIndex: {
+      0: "0",
+      10: "10",
+      20: "20",
+      30: "30",
+      40: "40",
+      50: "50",
+      60: "60",
+      70: "70",
+      80: "80",
+      90: "90",
+      100: "100",
+    },
   },
+  plugins: [
+    // popular tailwind plugins
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };
