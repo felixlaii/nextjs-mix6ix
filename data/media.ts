2d157071e58f299436a641d6b2310748812cf53f
Felix Lai <felix.lai@hotmail.com>
2023-06-20 19:11:17 -0400

    rendered other videos on the list

--- data/media.ts
+++ data/media.ts
@@ -19,64 +19,64 @@ export const VideoData: {
     videoSrc:
       "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033455/mix_6ix_imperium_zt36ht.mp4",
   },
-  // {
-  //   id: "4",
-  //   videoSrc:
-  //     "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033451/mix_6ix_be_yourself_mqovgg.mp4",
-  // },
-  // {
-  //   id: "5",
-  //   videoSrc:
-  //     "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033450/221029-CDLM-Halloween-MIX6IX-Reel-003_tr604u.mp4",
-  // },
-  // {
-  //   id: "6",
-  //   videoSrc:
-  //     "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033447/221029-CDLM-Halloween-MIX6IX-Reel-001_ewcheh.mp4",
-  // },
-  // {
-  //   id: "7",
-  //   videoSrc:
-  //     "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033450/221029-CDLM-Halloween-MIX6IX-Reel-003_tr604u.mp4",
-  // },
-  // {
-  //   id: "8",
-  //   videoSrc:
-  //     "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033447/221029-CDLM-Halloween-MIX6IX-Reel-001_ewcheh.mp4",
-  // },
-  // {
-  //   id: "9",
-  //   videoSrc:
-  //     "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033442/221029-CDLM-Halloween-Reel-001_fpiiin.mp4",
-  // },
-  // {
-  //   id: "10",
-  //   videoSrc:
-  //     "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033437/221029-CDLM-Halloween-Reel-002_xwak5a.mp4",
-  // },
-  // {
-  //   id: "11",
-  //   videoSrc:
-  //     "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033416/export_1671484848077_qvzhmq.mov",
-  // },
-  // {
-  //   id: "12",
-  //   videoSrc:
-  //     "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033411/mix_6ix_ghost_voices_ti1kez.mp4",
-  // },
-  // {
-  //   id: "13",
-  //   videoSrc:
-  //     "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033378/57e51082-2301-4f1a-802a-2829dfa0926b_zaardi.mp4",
-  // },
-  // {
-  //   id: "14",
-  //   videoSrc:
-  //     "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033353/Mix_6ix_Canada_Day_2_tptuep.mov",
-  // },
-  // {
-  //   id: "15",
-  //   videoSrc:
-  //     "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033345/Mix_6ix_Canada_3LAU_yz6c28.mov",
-  // },
+  {
+    id: 4,
+    videoSrc:
+      "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033451/mix_6ix_be_yourself_mqovgg.mp4",
+  },
+  {
+    id: 5,
+    videoSrc:
+      "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033450/221029-CDLM-Halloween-MIX6IX-Reel-003_tr604u.mp4",
+  },
+  {
+    id: 6,
+    videoSrc:
+      "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033447/221029-CDLM-Halloween-MIX6IX-Reel-001_ewcheh.mp4",
+  },
+  {
+    id: 7,
+    videoSrc:
+      "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033450/221029-CDLM-Halloween-MIX6IX-Reel-003_tr604u.mp4",
+  },
+  {
+    id: 8,
+    videoSrc:
+      "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033447/221029-CDLM-Halloween-MIX6IX-Reel-001_ewcheh.mp4",
+  },
+  {
+    id: 9,
+    videoSrc:
+      "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033442/221029-CDLM-Halloween-Reel-001_fpiiin.mp4",
+  },
+  {
+    id: 10,
+    videoSrc:
+      "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033437/221029-CDLM-Halloween-Reel-002_xwak5a.mp4",
+  },
+  {
+    id: 11,
+    videoSrc:
+      "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033416/export_1671484848077_qvzhmq.mov",
+  },
+  {
+    id: 12,
+    videoSrc:
+      "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033411/mix_6ix_ghost_voices_ti1kez.mp4",
+  },
+  {
+    id: 13,
+    videoSrc:
+      "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033378/57e51082-2301-4f1a-802a-2829dfa0926b_zaardi.mp4",
+  },
+  {
+    id: 14,
+    videoSrc:
+      "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033353/Mix_6ix_Canada_Day_2_tptuep.mov",
+  },
+  {
+    id: 15,
+    videoSrc:
+      "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033345/Mix_6ix_Canada_3LAU_yz6c28.mov",
+  },
 ];
--- src/pages/media.tsx
+++ src/pages/media.tsx
@@ -1,94 +1,47 @@
-import React, { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { VideoData } from "../../data/media";
-import VideoThumbnail from "react-video-thumbnail";
-import "bootstrap/dist/css/bootstrap.css";
+import VideoList from "@/components/VideoList";
 
 const Media: React.FC = () => {
-  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const [selectedVideo, setSelectedVideo] = useState(VideoData[0]);
+  const [otherVideos, setOtherVideos] = useState(
+    VideoData.filter((video) => video.id !== selectedVideo.id)
+  );
 
-  const handleThumbnailClick = (index: number) => {
-    const previousVideoIndex = currentVideoIndex;
-    setCurrentVideoIndex(index);
-
-    // Remove the clicked video from the VideoData array
-    const updatedVideoData = VideoData.filter((_, i) => i !== index);
-    VideoData.splice(0, VideoData.length, ...updatedVideoData);
-  };
-
-  const generateThumbnail = async (videoSrc: string): Promise<string> => {
-    // Replace this code with your actual thumbnail generation logic
-    // For example, you can use a third-party library or service to generate thumbnails
-    // and return the thumbnail URL
-    // Here, we're returning a placeholder URL
-    return "https://res.cloudinary.com/dyjj9jfc2/video/upload/v1687033460/mix_6ix_genesis_ho7bbz.mp4";
-  };
-  const generateThumbnails = async () => {
-    for (let i = 0; i < VideoData.length; i++) {
-      const video = VideoData[i];
-      const thumbnail = await generateThumbnail(video.videoSrc);
-      video.thumbnail = thumbnail; // Update the thumbnail field in the VideoData array
-      video.showThumbnail = true; // Initialize the showThumbnail property
-    }
+  const handleVideoSelect = (video: (typeof VideoData)[number]) => {
+    setSelectedVideo(video);
+    setOtherVideos(VideoData.filter((v) => v.id !== video.id));
   };
 
   useEffect(() => {
-    generateThumbnails();
+    setSelectedVideo(VideoData[0]);
+    setOtherVideos(VideoData.slice(1));
   }, []);
 
-  const handleMouseEnter = (e: React.MouseEvent<HTMLVideoElement>) => {
-    const vid = e.target as HTMLVideoElement;
-    vid.muted = true;
-    vid.play();
-  };
-
-  const handleMouseLeave = (e: React.MouseEvent<HTMLVideoElement>) => {
-    const vid = e.target as HTMLVideoElement;
-    vid.muted = false;
-    vid.currentTime = 0;
-    vid.pause();
-  };
-
-  const currentVideo = VideoData[currentVideoIndex];
-
   return (
-    <div className="flex">
-      <div className="w-1/2">
-        {currentVideo.showThumbnail ? (
-          <img
-            className="w-96 h-96"
-            src={currentVideo.thumbnail}
-            alt="Thumbnail"
-          />
-        ) : (
-          <video className="w-96 h-96" src={currentVideo.videoSrc} controls />
-        )}
+    <>
+      <div className="primary-video">
+        <video
+          className="primary-video__video"
+          src={selectedVideo.videoSrc}
+          controls
+        />
       </div>
-      <div className="w-1/4">
-        <ul>
-          {VideoData.map((video, index) => (
-            <li key={video.id} onClick={() => handleThumbnailClick(index)}>
-              {video.showThumbnail ? (
-                <img
-                  className="w-20 h-25"
-                  src={video.thumbnail}
-                  alt="Thumbnail"
-                />
-              ) : (
-                <video
-                  width="320"
-                  height="240"
-                  controls
-                  onMouseEnter={handleMouseEnter}
-                  onMouseLeave={handleMouseLeave}
-                >
-                  <source src={video.videoSrc} type="video/mp4" />
-                </video>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+
+      <ul>
+        {otherVideos.map((video) => (
+          <li
+            className="w-100"
+            key={video.id}
+            onClick={() => handleVideoSelect(video)}
+          >
+            <video width="320" height="240" controls>
+              <source src={video.videoSrc} type="video/mp4" />
+            </video>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
