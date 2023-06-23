dbae181aede24fe7d92212994b7afa973ad09ba3
Felix Lai <felix.lai@hotmail.com>
2023-06-23 16:04:10 -0400

    changed bg image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,13 +5,16 @@ import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="bg-brand-darkest">
+    <>
       <Head>
         <title></title>
       </Head>
+      {/* <div className="flex justify-center h-full w-screen bg-[url('https://res.cloudinary.com/dyjj9jfc2/image/upload/v1687549204/scott-webb-UjupleczBOY-unsplash_gumsqq.jpg')]"> */}
       <ProjectLayout>
         <Component {...pageProps} />
       </ProjectLayout>
-    </div>
+      {/* </div> */}
+      {/* <div className="absolute inset-0 bg-brand-base/90 to-black mix-blend-multiply" /> */}
+    </>
   );
 }
