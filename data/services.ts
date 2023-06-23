83d950a910ce0f9b3f2dcfd8a42d11bef7e8ed5d
Felix Lai <felix.lai@hotmail.com>
2023-06-23 18:58:55 -0400

    adjusted flex

--- data/services.ts
+++ data/services.ts
@@ -9,7 +9,7 @@ export const MIX_SERVICES: {
     service: "Planning",
     href: "/services#planning",
     id: "planning",
-    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1684958271/premium_photo-1674374441557-db209423bbae_xqaxto.avif",
+    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1687031905/drinks-image-01_vkxvvb.jpg",
     description:
       "MIX6IX aims for cocktail perfection and unforgettable experiences. From initial inquiry to event attendance, we offer personalized consultations and a menu of handcrafted cocktails. Share your vision, and we'll collaborate to create the perfect bar for any occasion.",
   },
@@ -17,7 +17,7 @@ export const MIX_SERVICES: {
     service: "Bartender/Mixologist Packages",
     href: "/services#bartender-packages",
     id: "bartender-packages",
-    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1684958271/premium_photo-1674374441557-db209423bbae_xqaxto.avif",
+    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1687031904/drinks-image-04_smj88a.jpg",
     description:
       "Find the perfect event package after a consultation. Our certified mixologists ensure a safe and entertaining experience for your guests. From setting up a full-service bar to handling all preparations, share your ideas, and we'll take care of the rest. With unbeatable prices in the GTA, choose us for your next party!",
   },
@@ -25,7 +25,7 @@ export const MIX_SERVICES: {
     service: "Mixology Workshop/Virtual Workshops",
     href: "/services#mixology-workshops",
     id: "bartender-packages",
-    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1684958271/premium_photo-1674374441557-db209423bbae_xqaxto.avif",
+    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1687031904/drinks-image-05_q1gcqz.jpg",
     description:
       "Join our Mixology Workshop for team building or private gatherings. This interactive session teaches essential cocktail-making skills. Explore classic recipes and unleash your creativity by crafting unique concoctions. Engage in a fun-filled experience for all!",
   },
@@ -34,7 +34,7 @@ export const MIX_SERVICES: {
     service: "Private Bartending 101 Class",
     href: "/services#bartending-101",
     id: "bartender-packages",
-    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1684958271/premium_photo-1674374441557-db209423bbae_xqaxto.avif",
+    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1687031904/drinks-image-02_imcrqe.jpg",
     description:
       "Master the art of bartending with our private training sessions. Whether you're a beginner or want to refine your skills, we customize the program to suit your needs. Learn free pours, techniques, and the significance of each bar tool. Gain the confidence and expertise to create classic cocktails like a pro. Enjoy private sessions focused on building your success behind the bar. Start your bartending career with us!",
   },
@@ -43,7 +43,7 @@ export const MIX_SERVICES: {
     service: "Custom Bar Menus",
     href: "/services#custom-menus",
     id: "bartender-packages",
-    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1684958271/premium_photo-1674374441557-db209423bbae_xqaxto.avif",
+    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1687031904/drinks-image-03_hcoy45.jpg",
     description:
       "Custom foiled bar menus designed by Blush & Letters co. to personalize and complete your unique experience. ",
   },
@@ -52,7 +52,7 @@ export const MIX_SERVICES: {
     service: "Servers/Support Staff and Bar Backs",
     href: "/services#support-staff",
     id: "bartender-packages",
-    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1684958271/premium_photo-1674374441557-db209423bbae_xqaxto.avif",
+    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1687031903/drinks-image-07_zqmuc3.jpg",
     description:
       "Need an extra hand at the party? We offer servers and support staff to help make your party run as smoothly as possible. All of our staff are experienced in the industry and love to interact with guests. ",
   },
@@ -61,7 +61,7 @@ export const MIX_SERVICES: {
     service: "Premium Cocktail Kits & Custom Cocktail Kits",
     href: "/services#cocktail-kits",
     id: "bartender-packages",
-    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1684958271/premium_photo-1674374441557-db209423bbae_xqaxto.avif",
+    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1687031903/drinks-image-08_in0xrw.jpg",
     description:
       "With the hard work of ingredient sourcing done for you, MIX6IX leaves little room for error for the cocktail enthusiasts at home. -no excessive sampling until after you've read the instructions! All that's left to do is stir, maybe shake, and sip away for a good time.   (TO ORDER: fill out submission form, send us a message on our Instagram page or e-mail us at mix6ixinfo@gmail.com)",
   },
--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -50,49 +50,100 @@ const ServiceDiv = ({ img, name, service, description }: ServiceImageProps) => {
 
 export const ServiceLinks: React.FC<ServiceLinkArr> = ({ links }) => {
   const [hoveredIndex, setHoveredIndex] = useState(-1);
+
+  const topImages = links.slice(0, 4); // Select the first 4 images
+  const bottomImages = links.slice(4, 7); // Select the next 3 images
+
   return (
-    <ul className="flex flex-wrap md:px-12 overflow-y-hidden xl:w-screen md:justify-center xl:gap-12 relative z-10">
-      {links.map((link, index) => (
-        <li
-          key={link.service}
-          className="flex w-1/2 md:w-1/5 justify-center xl:w-fit md:hover:scale-110 md:ease-in-out md:duration-200 md:pt-12 mx-auto md:mx-0 relative"
-          onMouseEnter={() => setHoveredIndex(index)}
-          onMouseLeave={() => setHoveredIndex(-1)}
-        >
-          <motion.div
-            className="box relative"
-            whileHover={{ scale: 1.1 }}
-            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+    <div className="flex md:flex-col md:flex-wrap md:justify-center md:items-center">
+      <div className="flex flex-row md:w-1/2">
+        {/* Render the top 4 images */}
+        {topImages.map((link, index) => (
+          <div
+            key={link.service}
+            className="flex justify-center xl:w-fit md:hover:scale-110 md:ease-in-out md:duration-200 md:pt-12 md:mx-30 relative"
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(-1)}
           >
-            {hoveredIndex === index ? (
-              <motion.p
-                className="font-extralight text-sm text-center py-4 xl:text-lg description"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.2 }}
-              >
-                {link.description}
-              </motion.p>
-            ) : (
-              <>
-                <div className="text-white absolute inset-0 flex items-center justify-center">
-                  <p className="font-bold text-xl text-center py-4 xl:text-lg">
-                    {link.service}
-                  </p>
-                </div>
-                <Link href={link.href}>
-                  <img
-                    src={link.img}
-                    alt={link.service}
-                    className="z-10 h-41 w-41 mx-auto object-cover hover:img-hover mt-3 aspect-w-1 aspect-h-1"
-                  />
-                </Link>
-              </>
-            )}
-          </motion.div>
-        </li>
-      ))}
-    </ul>
+            <motion.div
+              className="box relative"
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            >
+              {hoveredIndex === index ? (
+                <motion.p
+                  className="font-extralight text-sm text-center text-white py-4 xl:text-lg description"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  {link.description}
+                </motion.p>
+              ) : (
+                <>
+                  <div className="text-white absolute inset-0 flex items-center justify-center">
+                    <p className="text-md text-center py-20 xl:text-lg">
+                      {link.service}
+                    </p>
+                  </div>
+                  <Link href={link.href}>
+                    <img
+                      src={link.img}
+                      alt={link.service}
+                      className="rounded-full z-10 h-40 w-40 mx-auto py-30 object-cover hover:img-hover mt-3"
+                    />
+                  </Link>
+                </>
+              )}
+            </motion.div>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex flex-row md:w-1/2">
+        {/* Render the bottom 3 images */}
+        {bottomImages.map((link, index) => (
+          <div
+            key={link.service}
+            className="flex justify-center xl:w-fit md:hover:scale-110 md:ease-in-out md:duration-200 md:pt-12 mx-auto md:mx-30 relative"
+            onMouseEnter={() => setHoveredIndex(index + 4)}
+            onMouseLeave={() => setHoveredIndex(-1)}
+          >
+            <motion.div
+              className="box relative"
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            >
+              {hoveredIndex === index + 4 ? (
+                <motion.p
+                  className="font-extralight text-sm text-center text-white py-4 xl:text-lg description"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  {link.description}
+                </motion.p>
+              ) : (
+                <>
+                  <div className="text-white absolute inset-0 flex items-center justify-center">
+                    <p className="font-bold text-xl text-center py-4 xl:text-lg">
+                      {link.service}
+                    </p>
+                  </div>
+                  <Link href={link.href}>
+                    <img
+                      src={link.img}
+                      alt={link.service}
+                      className="rounded-full z-10 h-40 w-40 mx-auto object-cover hover:img-hover mt-3"
+                    />
+                  </Link>
+                </>
+              )}
+            </motion.div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
