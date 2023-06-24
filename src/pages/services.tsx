ff0695af58137f4a18e8b8b1495fe49222628d27
Felix Lai <felix.lai@hotmail.com>
2023-06-24 13:36:52 -0400

    Update services.tsx

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -161,14 +161,10 @@ const Services: React.FC = () => {
   }, [controls, inView]);
   return (
     <div className="w-[calc(10% - 10px)] mx-27 xl:mx-40 relative z-10 animate-fadeInFast transition">
-      <h1 className="text-center pt-24 sm:pt-0 my-12 font-semibold text-2xl xl:text-3xl text-shadow-lg shadow-zinc-300">
-        Services
+      <h1 className="text-center text-white pt-24 sm:pt-0 my-12 font-semibold text-2xl xl:text-3xl text-shadow-lg shadow-zinc-300">
+        Our Services
       </h1>
-      <p className="font-extralight text-center mb-12 xl:max-w-xl xl:mx-auto xl:text-xl">
-        Technology has revolutionized dental care, and the services we offer are
-        at the cutting-edge of dentistry. No matter how small or large your
-        problem is, or if you only need dental maintenance, we can help.
-      </p>
+
       <div className="mb-12">
         <ServiceLinks links={MIX_SERVICES} />
       </div>
