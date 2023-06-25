ce6eca63d753980bc3a139291bfd2bf50d63c819
Felix Lai <felix.lai@hotmail.com>
2023-06-25 16:38:41 -0400

    added scroll to more videos and edited heights

--- src/pages/media.tsx
+++ src/pages/media.tsx
@@ -6,7 +6,6 @@ const Media: React.FC = () => {
   const [otherVideos, setOtherVideos] = useState(
     VideoData.filter((video) => video.id !== selectedVideo.id)
   );
-  const [showMore, setShowMore] = useState(false);
 
   const handleVideoSelect = (video: (typeof VideoData)[number]) => {
     setSelectedVideo(video);
@@ -15,50 +14,40 @@ const Media: React.FC = () => {
 
   useEffect(() => {
     setSelectedVideo(VideoData[0]);
-    setOtherVideos(VideoData.slice(1, 5)); // Initially show only four videos
+    setOtherVideos(VideoData.slice(1));
   }, []);
-
-  const handleShowMore = () => {
-    setOtherVideos(VideoData.slice(1)); // Show all remaining videos
-    setShowMore(true); // Set the flag to indicate that all videos are being shown
-  };
 
   return (
     <div className="flex">
       <div className="mt-80 ml-40">
         <video
-          className="aspect-w-25 aspect-h-30 border border-gray-200 rounded-lg dark:border-gray-700"
+          className="h-[50rem] w-[100rem] border border-gray-200 dark:border-gray-700"
           controls
           preload="auto"
           src={selectedVideo.videoSrc}
         />
       </div>
 
-      <ul className="mt-72 mr-40 w-full h-full flex flex-col items-center justify-center">
+      <div className="mt-72 mr-40 w-full h-full flex flex-col items-center justify-center">
         <h1 className="text-white">Other Videos</h1>
 
-        {otherVideos.map((video) => (
-          <li
-            className="m-5"
-            key={video.id}
-            onClick={() => handleVideoSelect(video)}
-          >
-            <video
-              className="w-64 h-48 border border-gray-200 rounded-lg dark:border-gray-700 object-cover object-top"
-              src={video.videoSrc}
-            />
-          </li>
-        ))}
-
-        {!showMore && otherVideos.length > 0 && (
-          <button
-            className="text-center text-white sm:pt-0 my-2 font-semibold text-ll xl:text-1xl text-shadow-lg shadow-zinc-300"
-            onClick={handleShowMore}
-          >
-            Show More
-          </button>
-        )}
-      </ul>
+        <div className="overflow-y-auto">
+          <ul className="w-80 h-[49rem]">
+            {otherVideos.map((video) => (
+              <li
+                className="m-5"
+                key={video.id}
+                onClick={() => handleVideoSelect(video)}
+              >
+                <video
+                  className="w-64 h-48 border border-gray-200  dark:border-gray-700 object-cover object-top"
+                  src={video.videoSrc}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
