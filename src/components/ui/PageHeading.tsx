25ba3b0c55546d2cd58ab5a5866369b7f451728a
Felix Lai <felix.lai@hotmail.com>
2023-06-25 17:00:29 -0400

    added page header

---
+++ src/components/ui/PageHeading.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+
+export interface PageHeadingProps {
+  title: string;
+}
+const PageHeading: React.FC<PageHeadingProps> = ({ title }) => {
+  return (
+    <>
+      <div className="mt-[12rem] flex justify-center h-full w-screen bg-[url('https://res.cloudinary.com/dyjj9jfc2/image/upload/v1687549205/frantisek-g-XXuVXLy5gHU-unsplash_ywkclz.jpg')]">
+        <h1 className="relative z-10 my-auto text-center px-4 text-3xl md:text-4xl xl:text-5xl text-white tracking-widest text-shadow-lg shadow-zinc-700">
+          {title}
+        </h1>
+      </div>
+      <div className="absolute inset-0 bg-brand-lighter/90 to-black mix-blend-multiply" />
+    </>
+  );
+};
+
+export default PageHeading;
--- src/pages/media.tsx
+++ src/pages/media.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { VideoData } from "../../data/media";
+import PageHeading from "@/components/ui/PageHeading";
 
 const Media: React.FC = () => {
   const [selectedVideo, setSelectedVideo] = useState(VideoData[0]);
@@ -18,37 +19,44 @@ const Media: React.FC = () => {
   }, []);
 
   return (
-    <div className="flex">
-      <div className="mt-80 ml-40">
-        <video
-          className="h-[50rem] w-[100rem] border border-gray-700 dark:border-gray-700"
-          controls
-          preload="auto"
-          src={selectedVideo.videoSrc}
-        />
+    <>
+      <div className="relative pt-10 sm:pt-0 h-[12rem] md:h-[16rem]">
+        <PageHeading title="Video Gallery" />
       </div>
+      <div className="flex">
+        <div className=" mt-[8rem] ml-40">
+          <video
+            className="h-[50rem] w-[100rem] border border-gray-700 dark:border-gray-700"
+            controls
+            preload="auto"
+            src={selectedVideo.videoSrc}
+          />
+        </div>
 
-      <div className="mt-72 mr-40 w-full h-full flex flex-col items-center justify-center">
-        <h1 className="text-white">Other Videos</h1>
+        <div className="mt-[8rem] mr-40 w-full h-full flex flex-col items-center justify-center">
+          <h1 className="text-white text-lg text-shadow-lg shadow-zinc-300">
+            Other Videos
+          </h1>
 
-        <div className="overflow-y-auto">
-          <ul className="w-80 h-[49rem]">
-            {otherVideos.map((video) => (
-              <li
-                className="m-5"
-                key={video.id}
-                onClick={() => handleVideoSelect(video)}
-              >
-                <video
-                  className="w-64 h-48 border border-gray-700  dark:border-gray-700 object-cover object-top"
-                  src={video.videoSrc}
-                />
-              </li>
-            ))}
-          </ul>
+          <div className="overflow-y-auto">
+            <ul className="w-80 h-[48rem]">
+              {otherVideos.map((video) => (
+                <li
+                  className="m-5"
+                  key={video.id}
+                  onClick={() => handleVideoSelect(video)}
+                >
+                  <video
+                    className="w-64 h-48 border border-gray-700  dark:border-gray-700 object-cover object-top"
+                    src={video.videoSrc}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
