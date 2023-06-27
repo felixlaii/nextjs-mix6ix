b6c0d53fa58eb4daeb9b0969fc196e21331168ca
Felix Lai <felix.lai@hotmail.com>
2023-06-27 19:20:47 -0400

    edited footer styles and changed icons

--- src/components/layout/project-layout/Footer.tsx
+++ src/components/layout/project-layout/Footer.tsx
@@ -1,42 +1,23 @@
 import Link from "next/link";
-import { BsFacebook, BsTiktok } from "react-icons/bs";
-import { MdOutlineContactPhone } from "react-icons/md";
-import {
-  AiOutlineMail,
-  AiOutlineMenu,
-  AiOutlinePhone,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { BiDrink } from "react-icons/bi";
+import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
 import { FooterProps } from "@/types/component-types";
 import { snakeCaseToTitleCase } from "@/lib/functions";
 import { MIX6IX_DETAILS } from "../../../../data/mix6ix-info";
+import {
+  PiTiktokLogoLight,
+  PiFacebookLogo,
+  PiInstagramLogoLight,
+} from "react-icons/pi";
 
-const iconClassName = "mt-1 mr-2 w-4 h-4 text-white";
-const divClassName = " flex flex-row justify-center mt-5 mb-1";
-const socialIconClassName = "mt-5 mb-3 mr-2 w-7 h-7 text-white";
+const iconClassName = "mr-2 w-4 h-4 text-white";
+const divClassName = " flex flex-row justify-center mt-2 mb-1";
+const socialIconClassName = "mt-3 mb-3 mr-4 w-7 h-7 text-brand-gold";
 
 const LogoFooter = () => {
   return (
-    <div className="h-full flex justify-center w-full">
-      <div className="relative my-auto">
-        <img className="h-[10rem]" src="/images/mix-shaker.png" alt="logo" />
-      </div>
-    </div>
-  );
-};
-
-const FooterHeading: React.FC<{ title: string; icon: any }> = ({
-  title,
-  icon,
-}) => {
-  return (
-    <div className="flex justify-center bg-white/20 w-full h-10">
-      <div className="flex relative my-auto">
-        <div>{icon}</div>
-        <h2 className="font-primary tracking-widest text-md text-white">
-          <strong>{title}</strong>
-        </h2>
+    <div>
+      <div className="relative">
+        <img className="h-[6.5rem]" src="/images/MIX6IX2022.png" alt="logo" />
       </div>
     </div>
   );
@@ -46,24 +27,22 @@ const FooterMenu: React.FC<Pick<FooterProps, "navigationLinks">> = ({
   navigationLinks,
 }) => {
   return (
-    <div className="flex flex-col justify-between h-full w-full bg-brand-lighter pb-5">
+    <div className="flex flex-row h-full w-full pb-5">
       <div className="relative text-center">
-        <FooterHeading
-          title="MENU"
-          icon={<AiOutlineMenu className="mt-1 mr-2 w-4 h-4 text-white" />}
-        />
-
-        <ul className="flex flex-col mx-auto text-md gap-y-2 font-light text-white mt-4">
+        <ul className="flex flex-row mx-auto text-md gap-y-2 font-light text-zinc-400 mt-4">
           {navigationLinks.map((link) => (
-            <li className="font-primary text-xs" key={link.name}>
-              {link.name ? (
+            <li
+              className="border rounded-full pr-5 pl-5 pt-2 pb-2 mx-5 font-primary text-[0.6em] tracking-widest"
+              key={link.name}
+            >
+              {link.name !== "FORMS" ? (
                 <div className="hover:font-medium">
                   <Link href={link.href}>
                     {snakeCaseToTitleCase(link.name)}
                   </Link>
                 </div>
               ) : (
-                <ul className="flex flex-col text-md gap-y-2">
+                <ul className="flex flex-row gap-y-2">
                   {link.dropdown?.map((drop) => (
                     <li key={drop.name} className="hover:font-medium">
                       <Link href={drop.href}>
@@ -84,25 +63,25 @@ const FooterMenu: React.FC<Pick<FooterProps, "navigationLinks">> = ({
 const Mix6ixSocials = () => {
   return (
     <div>
-      <FooterHeading
-        title="SOCIALS"
-        icon={<BiDrink className={iconClassName} />}
-      />
-      <div className="flex flex-col items-center">
+      <div className="flex flex-row mb-1">
         <div>
           <a href="https://www.instagram.com/mix.6ix/" target="_blank">
             {" "}
-            <AiFillInstagram className={socialIconClassName} />
+            <PiInstagramLogoLight className={socialIconClassName} />
           </a>
+        </div>
+        <div>
           <a
             href="https://www.tiktok.com/@mix.6ix?_t=8dTWCTllNv4&_r=1"
             target="_blank"
           >
-            <BsTiktok className={socialIconClassName} />
+            <PiTiktokLogoLight className={socialIconClassName} />
           </a>
+        </div>
+        <div>
           <a href="https://www.facebook.com/MIXX6IX" target="_blank">
             {" "}
-            <BsFacebook className={socialIconClassName} />
+            <PiFacebookLogo className={socialIconClassName} />
           </a>
         </div>
       </div>
@@ -112,16 +91,12 @@ const Mix6ixSocials = () => {
 
 const Mix6ixContact = () => {
   return (
-    <div className="flex flex-col justify-between h-full border-t-8 md:border-none border-brand-lightest bg-brand-lighter md:bg-brand-darkest">
-      <FooterHeading
-        title="CONTACT"
-        icon={<MdOutlineContactPhone className={iconClassName} />}
-      />
-      <div className="pb-8 pt-4 md:pt-0">
+    <div>
+      <div className="pb-1">
         <div className={divClassName}>
           <AiOutlinePhone className={iconClassName} />
           <a
-            className="hover:underline text-sm text-white"
+            className="hover:underline font-primary text-sm text-zinc-300 tracking-widest"
             href={`tel:${MIX6IX_DETAILS.phone}`}
           >
             {MIX6IX_DETAILS.phone}
@@ -130,7 +105,7 @@ const Mix6ixContact = () => {
         <div className={divClassName}>
           <AiOutlineMail className={iconClassName} />
           <a
-            className="font-primary text-xs hover:underline text-white"
+            className="font-primary text-xs hover:underline tracking-widest text-zinc-300"
             href={`mailto:${MIX6IX_DETAILS.email}`}
           >
             {MIX6IX_DETAILS.email}
@@ -142,18 +117,29 @@ const Mix6ixContact = () => {
 };
 const Footer: React.FC<FooterProps> = ({ navigationLinks }) => {
   return (
-    <div className="flex flex-col-reverse md:grid md:grid-cols-4 h-full mt-48">
-      <div className="hidden md:inline-block">
-        <LogoFooter />
-      </div>
-      <div className="hidden md:inline-block">
-        <FooterMenu navigationLinks={navigationLinks} />
-      </div>
-      <div>
-        <Mix6ixContact />
-      </div>
-      <div>
-        <Mix6ixSocials />
+    <div className="h-full mt-48">
+      <div className="flex flex-col items-center justify-center">
+        <div className="hidden md:inline-block">
+          <LogoFooter />
+        </div>
+        {/* <div className="flex flex-col justify-center w-1/2">
+          <p className="font-primary text-xs text-white">
+            Passionate hospitality professionals specializing in crafting
+            memorable cocktail experiences. We offer exclusive, personalized
+            services, embodying a culture of craftsmanship. From intimate
+            gatherings to grand celebrations, we are the trusted bartending
+            company in the Greater Toronto Area.
+          </p>
+        </div> */}
+        <div>
+          <Mix6ixContact />
+        </div>
+        <div>
+          <Mix6ixSocials />
+        </div>
+        <div>
+          <FooterMenu navigationLinks={navigationLinks} />
+        </div>
       </div>
     </div>
   );
