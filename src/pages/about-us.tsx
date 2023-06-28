dae854abd850d9af17a62b92eea48c56f4e21569
Felix Lai <felix.lai@hotmail.com>
2023-06-27 20:28:34 -0400

    added new about me pic

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -1,33 +1,26 @@
 const AboutUs: React.FC = () => {
   return (
-    <div className="flex w-[calc(10% - 10px)] mx-8 gap-y-10 xl:mx-0 xl:w-screen">
-      <div className="mt-56">
-        <div className="flex justify-between relative w-screen">
-          <div>
-            <div className="absolute rounded-full w-[5rem] h-[5rem] bg-zinc-500 -bottom-[-16rem] -left-[-17rem] opacity-40"></div>
+    <div className="w-[calc(10% - 10px)] mx-8 mt-[40rem] flex flex-row gap-y-10 xl:mx-0 xl:w-screen relative z-10">
+      <div className="xl:flex xl:flex-row md:mt-12 xl:-mt-40">
+        <div className="mr-10 ml-10 flex flex-row">
+          {/* <h1 className="text-md text-white font-primary tracking-widest">
+            About Us
+          </h1> */}
 
-            <img
-              className="rounded-full w-[20rem] h-[20rem] ml-10 opacity-70"
-              src="./images/sharon.JPG"
-              alt="blk logo"
-            />
+          <p className="text-zinc-500 text-lg tracking-widest font-primary w-1/2 border p-10">
+            A team of passionate, energetic, and experienced hospitality
+            professionals who specialize in creating the most memorable cocktail
+            experience for our guests. Our vision is to offer a culture through
+            craftsmanship, and a personalized and exclusive experience. From
+            intimate gatherings to large celebration parties. We are a
+            bartending company serving within the Greater Toronto Area.
+          </p>
 
-            <div className="absolute rounded-full w-[10rem] h-[10rem] bg-zinc-500 -bottom-50 -top-[-15rem] -left-[-1rem] opacity-40"></div>
-          </div>
-          <div className="mr-10 w-1/3">
-            <h1 className="text-md text-white font-primary font-bold">
-              About Us
-            </h1>
-            <p className="text-white text-sm font-primary ">
-              A team of passionate, energetic, and experienced hospitality
-              professionals who specialize in creating the most memorable
-              cocktail experience for our guests. Our vision is to offer a
-              culture through craftsmanship, and a personalized and exclusive
-              experience. From intimate gatherings to large celebration parties.
-              We are a bartending company serving within the Greater Toronto
-              Area.
-            </p>
-          </div>
+          <img
+            className="border w-1/2"
+            src="https://res.cloudinary.com/dyjj9jfc2/image/upload/v1687909604/about-us_zkerh3.jpg"
+            alt="aboutus"
+          />
         </div>
       </div>
     </div>
