1c25622c099fc8cb3aa64d2d6326682bc502f6a3
Felix Lai <felix.lai@hotmail.com>
2023-07-02 16:50:55 -0400

    main controls

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -7,15 +7,24 @@ interface Props {
 }
 
 const Reveal = ({ children, width = "fit-content" }: Props) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView]);
   return (
-    <div className="rlative overflow:hidden" style={{ width }}>
+    <div ref={ref} className="rlative overflow:hidden" style={{ width }}>
       <motion.div
         variants={{
           hidden: { opacity: 0, y: 75 },
           visible: { opacity: 1, y: 0 },
         }}
         initial="hidden"
-        animate="visible"
+        animate={mainControls}
         transition={{ duration: 0.5, delay: 0.25 }}
       >
         {children}
