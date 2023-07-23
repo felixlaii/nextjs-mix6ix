5776ad33b2dd41d4849f53772e1ab31197906949
Felix Lai <felix.lai@hotmail.com>
2023-07-23 16:43:34 -0400

    created animation

---
+++ src/components/Animation.tsx
@@ -0,0 +1,92 @@
+interface Transition {
+  duration: number;
+  ease: [number, number, number, number];
+}
+
+const transition: Transition = { duration: 1, ease: [0.76, 0, 0.24, 1] };
+
+export const opacity = {
+  initial: {
+    opacity: 0,
+  },
+  open: {
+    opacity: 1,
+    transition: { duration: 0.35 },
+  },
+  closed: {
+    opacity: 0,
+    transition: { duration: 0.35 },
+  },
+};
+
+export const height = {
+  initial: {
+    height: 0,
+  },
+  enter: {
+    height: "auto",
+    transition,
+  },
+  exit: {
+    height: 0,
+    transition,
+  },
+};
+
+export const background = {
+  initial: {
+    height: 0,
+  },
+  open: {
+    height: "100vh",
+    transition,
+  },
+  closed: {
+    height: 0,
+    transition,
+  },
+};
+
+export const blur = {
+  initial: {
+    filter: "blur(0px)",
+    opacity: 1,
+  },
+  open: {
+    filter: "blur(4px)",
+    opacity: 0.6,
+    transition: { duration: 0.3 },
+  },
+  closed: {
+    filter: "blur(0px)",
+    opacity: 1,
+    transition: { duration: 0.3 },
+  },
+};
+
+interface TranslateAnimation {
+  y: string | number;
+  opacity: number;
+  transition: {
+    duration: number;
+    ease: [number, number, number, number];
+    delay: number;
+  };
+}
+
+export const translate = {
+  initial: {
+    y: "100%",
+    opacity: 0,
+  },
+  enter: (i: [number, number]): TranslateAnimation => ({
+    y: 0,
+    opacity: 1,
+    transition: { duration: 1, ease: [0.76, 0, 0.24, 1], delay: i[0] },
+  }),
+  exit: (i: [number, number]): TranslateAnimation => ({
+    y: "100%",
+    opacity: 0,
+    transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: i[1] },
+  }),
+};
